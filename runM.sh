6629a08e7b2a822529f7d9ade4e9b6574b5cee42
Paweł Rubach <pawel.rubach@sorcersoft.com>
2017-12-18 11:39:36 +0100

    Fixed runM.sh, switched SQLite to work in-memory only

--- runM.sh
+++ runM.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-//screen -dmS bridge java -jar build/libs/WebBridgeBidding.war
+#screen -dmS bridge java -jar build/libs/WebBridgeBidding.war
 
-cd build/libs/
 java -jar build/libs/WebBridgeBidding.war
