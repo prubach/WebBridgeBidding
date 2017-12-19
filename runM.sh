55e7a9f33145a2645eadbb5171590558a751665c
magdalena <miwilska@o2.pl>
2017-12-19 03:04:58 +0100

    bidds (natural) and my version of runM. but oslo probably wrong

--- runM.sh
+++ runM.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #screen -dmS bridge java -jar build/libs/WebBridgeBidding.war
 
-java -jar build/libs/WebBridgeBidding.war
+cd build/libs/
+chmod +x WebBridgeBidding.war
+java -jar WebBridgeBidding.war
+
+
