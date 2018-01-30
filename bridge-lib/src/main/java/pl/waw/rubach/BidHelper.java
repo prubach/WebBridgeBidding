b75d9bf6d30041a383df45ab69ae88bd64d6ac87
Paweł Rubach <pawel.rubach@sorcersoft.com>
2018-01-30 18:58:22 +0100

    Fixing font coloring

--- bridge-lib/src/main/java/pl/waw/rubach/BidHelper.java
+++ bridge-lib/src/main/java/pl/waw/rubach/BidHelper.java
@@ -11,22 +11,22 @@ public class BidHelper {
      */
     public static String replaceSuitsInDesc(String desc) {
         if (desc == null) return null;
-        desc = desc.replaceAll("kier", " <font color=\"red\">\u2665</font color> ");
-        desc = desc.replaceAll("karo", " <font color=\"red\">\u2666</font color> ");
-        desc = desc.replaceAll("trefl", " <font color=\"black\">\u2663</font color> ");
-        desc = desc.replaceAll("pik", " <font color=\"black\">\u2660</font color> ");
+        desc = desc.replaceAll("kier", " <font color=\"red\">\u2665</font> ");
+        desc = desc.replaceAll("karo", " <font color=\"red\">\u2666</font> ");
+        desc = desc.replaceAll("trefl", " <font color=\"black\">\u2663</font> ");
+        desc = desc.replaceAll("pik", " <font color=\"black\">\u2660</font> ");
 
-        desc = desc.replaceAll("kiery", " <font color=\"red\">\u2665</font color> ");
-        desc = desc.replaceAll("kara", " <font color=\"red\">\u2666</font color> ");
-        desc = desc.replaceAll("trefle", " <font color=\"black\">\u2663</font color> ");
-        desc = desc.replaceAll("piki", " <font color=\"black\">\u2660</font color> ");
+        desc = desc.replaceAll("kiery", " <font color=\"red\">\u2665</font> ");
+        desc = desc.replaceAll("kara", " <font color=\"red\">\u2666</font> ");
+        desc = desc.replaceAll("trefle", " <font color=\"black\">\u2663</font> ");
+        desc = desc.replaceAll("piki", " <font color=\"black\">\u2660</font> ");
 
-        desc = desc.replaceAll("kierów", " <font color=\"red\">\u2665</font color> ");
-        //  desc = desc.replaceAll("kar", " <font color=\"red\">\u2666</font color> ");
-        desc = desc.replaceAll("trefli", " <font color=\"black\">\u2663</font color> ");
-        desc = desc.replaceAll("pików", " <font color=\"black\">\u2660</font color> ");
+        desc = desc.replaceAll("kierów", " <font color=\"red\">\u2665</font> ");
+        //  desc = desc.replaceAll("kar", " <font color=\"red\">\u2666</font> ");
+        desc = desc.replaceAll("trefli", " <font color=\"black\">\u2663</font> ");
+        desc = desc.replaceAll("pików", " <font color=\"black\">\u2660</font> ");
 
-        //desc = desc.replaceAll("SK", "<font color=\"black\">\u2660</font color>");
+        //desc = desc.replaceAll("SK", "<font color=\"black\">\u2660</font>");
 
         return desc;
     }
@@ -40,13 +40,13 @@ public class BidHelper {
     public static String getBidSuit(String bidSuit) {
         switch (bidSuit) {
             case "C":
-                return "<font color=\"black\">\u2663</font color>";
+                return "<font color=\"black\">\u2663</font>";
             case "D":
-                return "<font color=\"red\">\u2666</font color>";
+                return "<font color=\"red\">\u2666</font>";
             case "H":
-                return "<font color=\"red\">\u2665</font color>";
+                return "<font color=\"red\">\u2665</font>";
             case "S":
-                return "<font color=\"black\">\u2660</font color>";
+                return "<font color=\"black\">\u2660</font>";
             case "NT":
                 return "BA";
         }
