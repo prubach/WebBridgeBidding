d3b461697b46ec984c492902aba4f3eade8a6bab
Paweł Rubach <pawel.rubach@gmail.com>
2021-04-27 12:27:58 +0200

    Fixing run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,20 @@
-#!/bin/bash
-screen -dmS bridge java -jar -Dspring.profiles.active=production build/libs/WebBridgeBidding.war
+#!/usr/bin/env bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#######
+PID=`jps | grep WebBridge | cut -f 1 -d " "`
+if [ -n "$PID" ]; then
+    kill $PID
+fi
+sleep 2
+PID=`jps | grep WebBridge | cut -f 1 -d " "`
+if [ -n "$PID" ]; then
+    kill -9 $PID
+fi
+cd $DIR
+screen -dmSL bridge java -jar -Dspring.profiles.active=production build/libs/WebBridgeBidding*.jar
