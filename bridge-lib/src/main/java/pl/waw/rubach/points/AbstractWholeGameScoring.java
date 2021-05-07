c76de9747450ea851383542b7f18daf7147c2a35
miwilska <22717834+miwilska@users.noreply.github.com>
2021-05-07 18:57:55 +0200

    copyright first try

--- bridge-lib/src/main/java/pl/waw/rubach/points/AbstractWholeGameScoring.java
+++ bridge-lib/src/main/java/pl/waw/rubach/points/AbstractWholeGameScoring.java
@@ -1,3 +1,26 @@
+/*
+ *
+ *  *                     GNU GENERAL PUBLIC LICENSE
+ *  *                        Version 0.8 ,  8. 05. 2021
+ *  *
+ *  *                   "Bridge-lib"  application as help
+ *  *                  for bidding, and points counting
+ *   *     Copyright (C) {2017}  {Paweł Rubach, Magdalena Wilska}
+ *  *
+ *  *     This program is free software: you can redistribute it and/or modify
+ *  *     it under the terms of the GNU General Public License as published by
+ *  *     the Free Software Foundation, either version 3 of the License, or
+ *  *      any later version.
+ *  *
+ *  *     This program is distributed in the hope that it will be useful,
+ *  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *     GNU General Public License for more details.
+ *  *
+ *  *
+ *
+ */
+
 package pl.waw.rubach.points;
 
 public abstract class AbstractWholeGameScoring extends AbstractOneDeal {
